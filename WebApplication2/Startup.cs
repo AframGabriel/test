17d3a40cac416d809bb50edfba34e80b56b9ca05
Afram Gabriel <afram.gabriel@hotmail.se>
2019-01-07 13:01:09 +0100

    zkjfgdfgjfglakj

--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -15,6 +15,7 @@ namespace WebApplication2
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //<zksjfadlkg
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
